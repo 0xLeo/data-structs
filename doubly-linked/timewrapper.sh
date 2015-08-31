89f9d6a45ff4b46b07c5d4cac2d510a12ddb91fd
Leontios Mavropalias <0xLeo@users.noreply.github.com>
2015-08-31 04:38:12 +0300

    fixed the name of executable
    from "doubly" to "test"

--- doubly-linked/timewrapper.sh
+++ doubly-linked/timewrapper.sh
@@ -5,7 +5,7 @@
 t=0; N=10
 for i in $(seq 1 $N)
 do
-t=$(echo "$t + " $(./doubly | grep -oP "[0-9]{1,2}\.[0-9]{4}")  | bc )
+t=$(echo "$t + " $(./test | grep -oP "[0-9]{1,2}\.[0-9]{4}")  | bc )
 done
 
 printf "OK. avg time = %.4f\n" $(echo "scale=4; $t/$N" | bc)
